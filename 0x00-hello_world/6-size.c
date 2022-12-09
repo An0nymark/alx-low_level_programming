9e9b421a3346489d032beaa8caf1a0971515a1f6
Mark <markmains484@gmail.com>
2022-12-09 12:47:13 -0800

    mm

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,19 +8,16 @@
 
 int main(void)
 {
-	int i;
-	double d;
-	char c;
 
-	printf("Size of a char: \n,"sizeof(char));
+	printf("Size of a char: %d byte(s)\n,", sizeof(char));
 
-	printf("Size of an int: \n"sizeof(int));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
 
-	printf("Size of a long int: \n"sizeof(long int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 
-	printf("Size of a long long int: \n"sizeof(long long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
-	printf("Size of a float: \n"sizeof(float));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
