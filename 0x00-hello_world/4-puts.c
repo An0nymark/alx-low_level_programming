f189607c86135f40f3fa7c5134b84df41eac1ceb
Mark <markmains484@gmail.com>
2022-12-09 08:15:27 -0800

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
 
-
-
-/**
+/*
+ * main - Mark
  *
- *  * main - KRIS
- *
- *   *  Return: AlwayTs 0
- *
- *    */
-
-
-
+ * Return:0
+ */
 int main(void)
-
-
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-					return (0);
-
-
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
