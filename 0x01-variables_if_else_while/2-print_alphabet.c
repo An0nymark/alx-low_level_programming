68c01c255f75573dd55d4f38ef0e23cbd6207fe1
Mark <markmains484@gmail.com>
2022-12-09 18:51:26 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ * main - alphabet game buana
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int ch = 'n';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
