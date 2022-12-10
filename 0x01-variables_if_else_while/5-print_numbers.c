a965ec97473f7ba75c8d4601c8f3a80b8840bc80
Mark <markmains484@gmail.com>
2022-12-10 03:21:47 -0800

    tsk

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ * main - main maina
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
