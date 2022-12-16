744fbb82a152f17e1c6241bd7d29113b52e41fe7
Mark <markmains484@gmail.com>
2022-12-16 14:56:38 -0800

    mm

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- *  _isupper - checks uppercase
- *  @c: parameter
- *  Return: 0 is success
+ * _isupper - checks uppercase
+ * @c: parameter
+ * Return: 0 is success
  */
-
 int _isupper(int c)
 {
-	if (c >= 65 && c < 91)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
+
 	else
-	{
 		return (0);
-	}
 }
